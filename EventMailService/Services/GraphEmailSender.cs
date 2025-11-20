b169afc04e280262ae624089dd89c63b5da33d48
Graham Synnott <gsynnott@linamar.com>
2025-11-20 09:00:26 -0500

    Add project files.

---
+++ EventMailService/Services/GraphEmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventMailService.Services
+{
+    internal class GraphEmailSender
+    {
+    }
+}
---
+++ EventMailService/Workers/PlcMonitorStub.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventMailService.Workers
+{
+    internal class PlcMonitorStub
+    {
+    }
+}
